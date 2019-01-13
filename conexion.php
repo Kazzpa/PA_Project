bd53cf33d09577b798043ce3b23b0d57520ae3b5
Irene González Ruiz <ireneglezruiz@gmail.com>
2019-01-13 20:08:27 +0100

    conexion a bd localhost y dominio

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "infinity"); //La ventaja de poner aqui la base de datos que es opcional esque nos ahorramos una sentencia 
+
+if (!$con) {
+    die("Conexion fallida: " . mysqli_connect_error()); /* Si la conexion ha fallado */
+}
+
+//$con = mysqli_connect("fdb24.awardspace.net", "2932955_infinity", "Infinity.123", "2932955_infinity"); //La ventaja de poner aqui la base de datos que es opcional esque nos ahorramos una sentencia 
+
+//if (!$con) {
+//    die("Conexion fallida: " . mysqli_connect_error()); /* Si la conexion ha fallado */
+//}
