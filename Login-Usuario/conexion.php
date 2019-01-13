0e7ef8a2b370042c93afacbaa7caef32e4c5ac79
Irene González Ruiz <ireneglezruiz@gmail.com>
2019-01-14 00:05:49 +0100

    arreglado conexion a bd

--- Login-Usuario/conexion.php
+++ Login-Usuario/conexion.php
@@ -5,3 +5,9 @@ $con = mysqli_connect("localhost", "root", "", "infinity"); //La ventaja de pone
 if (!$con) {
     die("Conexion fallida: " . mysqli_connect_error()); /* Si la conexion ha fallado */
 }
+
+//$con = mysqli_connect("fdb24.awardspace.net", "2932955_infinity", "Infinity.123", "2932955_infinity"); //La ventaja de poner aqui la base de datos que es opcional esque nos ahorramos una sentencia 
+
+//if (!$con) {
+//    die("Conexion fallida: " . mysqli_connect_error()); /* Si la conexion ha fallado */
+//}
