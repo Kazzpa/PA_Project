6d07af7c24b54778843b9b207be5e11cf3c1acee
Ander Lakidain de Arriba <lakidainander@gmail.com>
2019-01-20 02:45:43 +0100

    Bug fix

--- Login-Usuario/cuentaLogin.php
+++ Login-Usuario/cuentaLogin.php
@@ -19,11 +19,16 @@ and open the template in the editor.
     <body> 
         <?php
         include("header.php");
-        if (!isset($_SESSION["login"])) {    //Si no hemos hecho login lo mostramos
+        //======================================================================
+        // LOGIN INICIO SESION
+        //======================================================================
+        if (!isset($_SESSION["login"])) {
             ?>
+            <!--Aplicacion Boostrap-->
+            <!--Columnas creadas para centrar, una row tiene 12 filas. Usamos 3 a la izquierda 6  ocupa el registro y 3 a la derecha-->
             <div class="container login-container">
                 <div class="row">
-                    <div class="col-md-3">  <!--Columnas creadas para centrar, una row tiene 12 filas asi que usamos 3 a la izquierda 6 wur ocupa el registro y 3 a la derecha-->
+                    <div class="col-md-3">  
                     </div>
                     <div class="col-md-6 login-form-1">
                         <h3>Formulario de login</h3>
@@ -44,26 +49,20 @@ and open the template in the editor.
                     </div>
                 </div>
             </div>
-            <!--
-                <form action = "cuentaLoginProcesamiento.php" method = "post">
-                    Usuario: <input type = "text" name = "username" required/><br /> <!--Preguntar por correo electronico en vez de usuario
-                    Password: <input type = "password" name = "password" required /> <br />
-                    <input type = "submit" value="Enviar" name = "login" />
-                </form>
-                <form action = "cuentaRegistro.php" method = "post">
-                    Si no esta registrado <input type="submit" value="Registrese" name="registro" /> <!--Boton que al pulsar te redirecciona a registro
-                </form>
-            -->
             <?php
-//En vez de un html como ponia en la practica hemos introducido un php para poder avisar del error de logueo
+            //Control de fallo de logueo
             if (isset($_SESSION["fallido"])) {
                 echo "Error al logearse";
             }
+            //Si el usuario se acaba de registrar mandamos un mensaje.
             if (isset($_SESSION["exito"])) {
                 unset($_SESSION["exito"]);
                 echo "Se ha registrado correctamente, pruebe a logearse con la cuenta creada";
             }
-        } else {    //Si ya estamos con la sesion iniciada le permitimos hacer cambios en los datos
+        } else {
+            //======================================================================
+            // PANEL DE CONTROL ESTANDO YA LOGUEADO
+            //======================================================================
             ?>
             <div class="container-fluid"> 
                 <div class="row">
@@ -71,6 +70,11 @@ and open the template in the editor.
                     </div>
                     <div class="col-sm-8 text-center well">
                         <div class="row">
+                            <!--
+                            -----------------------------------------------------
+                            Panel de control, datos personales
+                            -----------------------------------------------------
+                            -->
                             <div class="col-sm-12 text-left well" style="background-color: black; color:black">
                                 <div class="col-sm-12 text-left well" style="background-color: white; color:black">
                                     <p style="font-size: 2em; text-align: center;">Datos personales</p>
@@ -85,6 +89,7 @@ and open the template in the editor.
                                     echo "<strong>Email:</strong> $email <br />";
                                     echo "<strong>Fecha Registro:</strong> $fechaRegistro <br />";
                                     ?>
+                                    <!-- Campo select modificacion de usuario -->
                                     <p> Si quiere modificar un dato seleccionelo del campo a continuacion e introduzca el nuevo valor: </p>
                                     <form action = "cuentaModificar.php" method = "post" enctype="multipart/form-data">
                                         <div id="zonaModificacionDatos">
@@ -100,48 +105,48 @@ and open the template in the editor.
                                         <br />
                                         <input id="botonModificacionDatos" type="submit" value="Modificar Dato" name="modificarDato" /> <!--Boton que al pulsar te redirecciona a registro-->
                                     </form>
-                                    <!--
-                                    <form action = "cuentaModificar.php" method = "post">
-                                        <select name="selectCuentaModificar">
-                                            <option value="email"> Email</option>
-                                            <option value="password"> Password</option>
-                                        </select>
-                                        <input type="text" value="" name="campoTexto"><br />
-                                        <input type="submit" value="Modificar Datos" name="registro" /> <!--Boton que al pulsar te redirecciona a registro
-                                    </form>
                                     <br />
-                                    <form action = "cuentaModificar.php" method = "post" enctype="multipart/form-data">
-                                        Si desea cambiar su imagen de perfil: <input type="file" name="imagen" /> <br />
-                                        <input type="submit" value="Modificar Foto" name="modificarFoto" /> <!--Boton que al pulsar te redirecciona a registro
-                                    </form>-->
-                                    <br />
+                                    <!-- Boton para el borrado de la cuenta-->
                                     <form action = "cuentaEliminar.php" method = "post">
                                         <input type="submit" value="Eliminar cuenta" name="registro" /> <!--Eliminamos la cuenta del usuario si asi lo pide-->
                                     </form>
                                 </div>
                             </div>
                         </div>
-
+                        <!--
+                        -----------------------------------------------------
+                        Panel de control, eventos
+                        -----------------------------------------------------
+                        -->
                         <div class="row">
                             <div class="col-sm-12 text-left well" style="background-color: rgb(36,182,148); color:black">
                                 <p style="font-size: 2em; text-align: center;">Eventos creados</p>
                                 <?php
-                                include_once("conexion.php");
+                                //Conexion a la base de datos
+                                include("conexion.php");
 
+                                //Consulta a la BD para extraer los eventos organizados por el usuario
                                 $username = $_SESSION["username"];
-                                $consulta = "SELECT * FROM `events` WHERE host = '$username'"; //consulta SQL para obtener el usuario, luego comprobamos la password
+                                $consulta = "SELECT * FROM `events` WHERE host = '$username'";
                                 $resultado = mysqli_query($con, $consulta);
 
-                                if (mysqli_num_rows($resultado) > 0) {    //si la consulta ha tenido exito podemos guardar en SESSION la informacion como que existe y el usuario esta logeado
+                                //Cerramos la conexion a la base de datos ya que no nos hace falta
+                                mysqli_close($con);
+
+                                //Si la consulta ha tenido exito
+                                if (!(mysqli_num_rows($resultado)) == 0) {
                                     $i = 0;
-                                    while ($auxiliar = mysqli_fetch_array($resultado)) { /* Con esto retiramos todas las filas que hayan en la base de datos, como pueden ser muchas
-                                      hay que ir leyendo fila a fila */
-                                        $fila[$i] = $auxiliar; //este auxiliar es para evitar que se meta la ultima iteracion vacia
+                                    //Retiramos las filas obtenidas en la consulta
+                                    while ($auxiliar = mysqli_fetch_array($resultado)) {
+                                        $fila[$i] = $auxiliar;
                                         $i++;
                                     }
                                     ?>
+
+                                    <!-- Campo select modificacion de evento -->
                                     <p>Modificaci&oacute;n de evento</p>
                                     <form id="formModificacionEvento" action = "eventoModificar.php" method = "post" enctype="multipart/form-data">
+                                        <!-- Colocamos en un campo select los eventos creados por el usuario-->
                                         <select name='selectEvento'>
                                             <?php
                                             for ($i = 0; $i < sizeof($fila); $i++) {
@@ -155,6 +160,7 @@ and open the template in the editor.
                                         </select>
                                         <br />
                                         <br />
+                                        <!-- Mostramos las opciones de modificacion del evento-->
                                         <div id="zonaModificacionDatosEvento">
                                             <select name="selectCampoEventoModificar" onchange="comprobarFormularioEventos()" id="opcionModificacionEvento">
                                                 <option value="name"> Nombre Evento</option>
@@ -167,7 +173,7 @@ and open the template in the editor.
                                             <br />
                                             <input type="text" name="campoTexto" placeholder="Nombre Evento" id="eventoACambiar">
 
-                                            <!--GOOGLE MAPS-->
+                                            <!--Bloque Google Maps mostrado al elegir la modificacion Localizacion-->
                                             <div id = "location" style="display:none">
                                                 <div id="map"></div>
                                                 <div class="pac-card" id="pac-card">
@@ -214,42 +220,6 @@ and open the template in the editor.
                                         <br />
                                         <input id="botonEventoModificar" type="submit" value="Modificar Evento" name="botonEventoModificar" />
                                     </form>
-
-                                    <!-- 
-                                    <p>Modificaci&oacute;n de evento</p>
-                                    <form action = "eventoModificar.php" method = "post">
-                     
-                                      for ($i = 0; $i < sizeof($fila); $i++) {
-                                      $event_id = $fila[$i]['id'];
-                                      $event_name = $fila[$i]['name'];
-                                      $event_date = $fila[$i]['date_celebration'];
-                                      $event_creation = $fila[$i]['date_creation'];
-                                      echo "<option value='$event_id'>Evento: $event_name, Fecha Celebracion: $event_date</option> ";
-                                      } */
-                                  
-                                    <!--</select>
-                                    <select name="selectCampoEventoModificar">
-                                        <option value="name"> Nombre Evento</option>
-                                        <option value="description"> Descripcion</option>
-                                    </select>
-                                    <textarea rows="4" cols="50" name="campoTexto"></textarea>
-                                    <input type="submit" value="Modificar Evento" name="botonEventoModificar" />
-                                </form>
-                                <br />
-                                <form action = "eventoModificar.php" method = "post" enctype="multipart/form-data">
-                                    <select name='selectEventoModificar'>
-
-                                      for ($i = 0; $i < sizeof($fila); $i++) {
-                                      $event_id = $fila[$i]['id'];
-                                      $event_name = $fila[$i]['name'];
-                                      $event_date = $fila[$i]['date_celebration'];
-                                      $event_creation = $fila[$i]['date_creation'];
-                                      echo "<option value='$event_id'>Evento: $event_name, Fecha Celebracion: $event_date</option> ";
-                                      } 
-                                    <!-- </select>
-                                     Si desea cambiar la imagen del evento: <input type="file" name="imagen" /> <br />
-                                     <input type="submit" value="Modificar Foto Evento" name="modificarFoto" />
-                                 </form> -->
                                 </div>
                             </div>
                         </div>
@@ -257,16 +227,10 @@ and open the template in the editor.
                         </div>
                     </div>
                 </div>
-
-
                 <?php
-            } else {
+            } else {    //Mensaje mostrado si no existen eventos creados
                 echo "No tiene eventos creados, animate y crea el tuyo!";
             }
-
-            mysqli_close($con); //Cerramos la conexion a la base de datos ya que no nos hace falta
-            ?>
-            <?php
         }
         ?>
         <script type="text/javascript">
